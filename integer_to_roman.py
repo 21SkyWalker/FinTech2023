8876de3cc941b703bb65d34f1741f8f32428a69a
21SkyWalker <73480799+21SkyWalker@users.noreply.github.com>
2023-09-25 10:29:16 +0300

    Create integer_to_roman.py

---
+++ integer_to_roman.py
@@ -0,0 +1,13 @@
+def int_to_roman(num):
+    roman_dict = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 
+                  100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 
+                  10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 
+                  1: 'I'}
+    roman_num = ''
+    for key in roman_dict.keys():
+        while num >= key:
+            roman_num += roman_dict[key]
+            num -= key
+    return roman_num
+
+print(int_to_roman(int(input())))
